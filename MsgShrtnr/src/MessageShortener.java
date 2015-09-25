7cd6c9a18e4ba4421320300bb6db5649842d0f99
Christopher Bong <christopherhb92@gmail.com>
2015-09-26 00:22:53 +1000

    Is now able to shorten messages according to a limited word dictionary in a textfile.

--- MsgShrtnr/src/MessageShortener.java
+++ MsgShrtnr/src/MessageShortener.java
@@ -1,19 +1,50 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.HashMap;
-
+import java.util.Scanner;
 
 public class MessageShortener {
-	private HashMap wordPairs;
+	private HashMap<String, String> wordPairs;
 	
 	public MessageShortener() {
-		this.wordPairs = new HashMap();
+		this.wordPairs = new HashMap<String, String>();
+		Scanner sc;
+		try {
+			sc = new Scanner(new FileReader("Dictionary.txt"));
+	    	String input;
+	    	String[] splitInput;
+	    	
+			// put all the words into a hashmap
+			while (sc.hasNext()) {
+		    	input = sc.nextLine();
+		    	splitInput = input.split("\\s+");
+		    	wordPairs.put(splitInput[0], splitInput[1]);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void shortenMessage(Message msg) {
 		String str = msg.getMsg();
 		
-	}
-	
-	private void checkString(String checkAgainst) {
+		// split the message
+		String[] words = str.split("\\s+");
+		String word;
+		for (int i = 0; i < words.length; i++) {
+			word = words[i].toLowerCase();
+			if (wordPairs.containsKey(word)) {
+				words[i] = wordPairs.get(word);
+			}
+		}
 		
+		// rebuild the message
+		StringBuilder resultMsg = new StringBuilder();
+		for(String s : words) {
+		    resultMsg.append(s);
+		    resultMsg.append(" ");
+		}
+		
+		msg.setMsg(resultMsg.toString());
 	}
 }
--- MsgShrtnr/src/WordPair.java
+++
@@ -1,5 +0,0 @@
-
-public class WordPair {
-	String fullWord;
-	String abbreviation;
-}
